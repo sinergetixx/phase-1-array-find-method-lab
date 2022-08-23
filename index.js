8612c970d2a9b3cabb462b095e37f68a12cd1b97
rob <robertmaticke12@gmail.com>
2022-08-23 18:22:08 -0400

    commit tree 6843c4670a8910e7fde137671000546260a7543b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(array){
+    let winYear = array.find(isWin)
+    return winYear ? winYear.year : undefined
+}
+
+function isWin(element) {
+    return element.result === 'W'
+}
